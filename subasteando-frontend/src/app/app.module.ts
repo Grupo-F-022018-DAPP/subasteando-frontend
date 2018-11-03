4a2d6c36aaa164ffc69594c1920579e6aac9609f
tini <agustinariq@gmail.com>
2018-11-03 20:37:05 -0300

    Creo Biddings. Borro archivos de test

--- subasteando-frontend/src/app/app.module.ts
+++ subasteando-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuctionsComponent } from './auctions/auctions.component';
 import { AuctionDetailComponent } from './auction-detail/auction-detail.component';
+import { BiddingsComponent } from './biddings/biddings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuctionsComponent,
-    AuctionDetailComponent
+    AuctionDetailComponent,
+    BiddingsComponent
   ],
   imports: [
     BrowserModule,
