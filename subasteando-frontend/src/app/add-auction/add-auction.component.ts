2dda0b62ad57c7ab0443d9eb03ce3dce40c69d58
tini <agustinariq@gmail.com>
2018-12-09 10:39:27 -0300

    Validacion de fecha de finalizacion a dos dias del inicio

--- subasteando-frontend/src/app/add-auction/add-auction.component.ts
+++ subasteando-frontend/src/app/add-auction/add-auction.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AuctionService } from '../auction.service';
 import { Auction } from '../auctions/auction'
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-auction',
@@ -9,6 +10,12 @@ import { Auction } from '../auctions/auction'
   styleUrls: ['./add-auction.component.css']
 })
 export class AddAuctionComponent implements OnInit {
+
+  @ViewChild('endDateID') dateInput: ElementRef;
+
+
+    minDate: Date;
+    endMinDate: Date;
 
   auction: Auction = {
     id: "",
@@ -26,13 +33,28 @@ export class AddAuctionComponent implements OnInit {
 
   }
 
-  constructor(private auctionService: AuctionService) { }
+  constructor(private auctionService: AuctionService) {
+    this.minDate = new Date();
+    this.endMinDate = new Date();
+
+   }
 
   ngOnInit() {
+    console.log("mira cabie")
   }
 
   saveAuction(auctionForm: NgForm) {
     this.auctionService.saveAuction(this.auction);
+  }
+
+  dateChanged(event) {
+    var newDate = event;
+    var twoDaysFromDate = newDate.setDate(newDate.getDate() + 2);
+    var datePipe = new DatePipe('en-US');
+    var formattedDate = datePipe.transform(twoDaysFromDate, 'MM/dd/yyyy');
+    var finalDate = new Date(formattedDate);
+    this.endMinDate = finalDate;
+    this.dateInput.nativeElement.value = "";
   }
 
 }
